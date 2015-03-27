0b75ffb360899a792f07bf2dc72364522358eef6
Joern Bernhardt <jb@campudus.com>
2015-03-27 11:12:54 +0100

    Simple 'module'
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

---
+++ calculator.js
@@ -0,0 +1,13 @@
+
+function addition(a, b) {
+  return a + b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+module.exports = {
+  plus : addition,
+  multiply : multiplication
+};
--- helloWorld.js
+++ helloWorld.js
@@ -1,25 +1,5 @@
 var fs = require('fs');
+var calculator = require('./calculator.js');
 
-console.log('hello world');
-
-var a = 1 + 2;
-var b = 3;
-
-console.log('a+b=' + a + b);
-
-fs.readFile('test.txt', 'utf8', read);
-console.log('fertig mit aktionen');
-
-function read(err, res) {
-  console.log('error=' + err);
-  if (err) {
-    console.log('error:' + err);
-  } else {
-    fs.writeFile('test2.txt', 'result=' + res, wrote);
-    console.log('io fertig=' + res);
-  }
-}
-
-function wrote(err) {
-  console.log('jetzt wirklich fertig');
-}
+console.log('5 + 15 = ' + calculator.plus(5, 15));
+console.log('5 * 15 = ' + calculator.multiply(5, 15));
