56dd7f95c3563a7afeb8d3c8be253ea80d92e3a7
Joern Bernhardt <jb@campudus.com>
2015-03-27 10:07:25 +0100

    Sync operation
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 
 console.log('hello world');
 
@@ -5,3 +6,10 @@ var a = 1 + 2;
 var b = 3;
 
 console.log('a+b=' + a + b);
+
+var i;
+
+for (i = 0; i < 20; i++) {
+  fs.appendFileSync('test.txt', 'hallo nochmal: '+i+'\n');
+}
+console.log('fertig');
