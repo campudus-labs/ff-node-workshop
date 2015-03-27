9806869f7c313fe8c3670eb7472f334ee8fe8926
Joern Bernhardt <jb@campudus.com>
2015-03-27 11:58:04 +0100

    Show off middleware
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var path = require('path');
-
 var app = express();
 
-app.use(function(req, res, next) {
+var helloRouter = express.Router();
+helloRouter.get('/olli', function (req, res, next) {
   console.log('got a request:' + req.path);
-  if (req.path === '/hello') {
-    res.send('hello!');
-  } else {
-    next();
-  }
+  res.sendFile(path.join(__dirname, 'ollis-spezial.html'));
 });
+helloRouter.get('/:name', function (req, res, next) {
+  console.log('got a request:' + req.path);
+  res.send('hello ' + req.params.name);
+});
+helloRouter.get('/', function (req, res, next) {
+  res.send('hello monsieur undefined');
+});
+
+app.use('/hello', helloRouter);
+
 app.use(express.static(path.join(__dirname, 'www')));
 
 app.listen(8080);
