0b0ceb0d2c8a231d5d7bfd22a5c4940ed0bd27b3
Joern Bernhardt <jb@campudus.com>
2015-03-27 12:16:44 +0100

    Add handlebars and show problems with sync
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
+var Handlebars = require('handlebars');
 var path = require('path');
+var fs = require('fs');
 var app = express();
+
+var hiTemplate;
+var templateFile = path.join(__dirname, 'www', 'something.html');
+fs.readFile(templateFile, 'utf8', function (err, result) {
+  setTimeout(function () {
+    hiTemplate = Handlebars.compile(result);
+  }, 10000);
+});
 
 var helloRouter = express.Router();
 helloRouter.get('/olli', function (req, res, next) {
@@ -9,13 +19,20 @@ helloRouter.get('/olli', function (req, res, next) {
 });
 helloRouter.get('/:name', function (req, res, next) {
   console.log('got a request:' + req.path);
-  res.send('hello ' + req.params.name);
+  res.send(hiTemplate({name : req.params.name}));
 });
 helloRouter.get('/', function (req, res, next) {
   res.send('hello monsieur undefined');
 });
 
 app.use('/hello', helloRouter);
+
+app.use('/something', function (req, res, next) {
+  var templateFile = path.join(__dirname, 'www', 'something.html');
+  fs.readFile(templateFile, 'utf8', function (err, result) {
+    res.send(Handlebars.compile(result)({name : 'konstantin'}));
+  });
+});
 
 app.use(express.static(path.join(__dirname, 'www')));
 
